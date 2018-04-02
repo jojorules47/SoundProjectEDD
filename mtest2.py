046e577fdc51a0d6ee109a2ba20b973416d8352d
Josiah Smith <jojorules4@gmail.com>
2018-04-02 13:56:17 -0600

    Got initial mtest2 working

--- mtest2.py
+++ mtest2.py
@@ -1,6 +1,7 @@
 import alsaaudio,time,audioop
 import matplotlib.pyplot as plt
-card = "hw:2,0"
+import numpy as np
+card = "hw:1,0"
 inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK,card)
 inp.setchannels(1)
 inp.setrate(32000)
@@ -8,22 +9,16 @@ inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
 inp.setperiodsize(160)
 
 stuff = 0
-thing = []
+buff = np.array([])
 test = True
-mtest = open("testfile.txt","w")
-for i in range(0,5000):
-    thing.append(0)
 
 while test == True:
     l,data = inp.read()
     if l:
-        thing[stuff] = audioop.max(data,2)
-        print(audioop.max(data,2))
-    time.sleep(.001)
+        numpydata = np.fromstring(data,dtype=np.int16)
+        buff = np.concatenate((buff,numpydata))
     stuff += 1
-    if stuff >= 5000:
+    if len(buff) >= 160000:
         test = False
-        mtest.write(str(thing))
-        mtest.close()
-        plt.plot(thing)
+        plt.plot(buff)
         plt.show()
